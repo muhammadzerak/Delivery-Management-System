17b4e6c804150db790e5f0fcede6d3e81a31f916
akashpawar43 <akashgp1643pawar+git@gmail.com>
2025-09-26 13:41:40 +0530

    server link added

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://delivery-management-system-lyart.vercel.app/api",
     withCredentials: true,
 });
 
